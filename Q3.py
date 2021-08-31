20add888b5b88283018dc612b13415877fd4b0ed
xdaniejoyy <daniellejoythio@gmail.com>
2021-08-31 14:07:07 +0800

    Q3

---
+++ Q3.py
@@ -0,0 +1,22 @@
+from scipy.special import comb
+
+
+def pascal_triangle(N):
+    try:
+        N = int(N)
+    except:
+        raise ValueError("N must be an integer.")
+
+    if N <= 0:
+        raise ValueError("N must be an int > 0.")
+
+    unique_values = (N - 1) // 2
+    vals = []
+    for k in range(unique_values + 1):
+        vals.append(comb(N-1, k))
+
+    if N % 2: # odd number, don't repeat last value
+        return vals + vals[-2::-1]
+    # even number, symmetrical list
+    return vals + vals[::-1]
+
