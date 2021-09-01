b419b2b314cc5b512e4cd187d185ad32e33584d8
xdaniejoyy <daniellejoythio@gmail.com>
2021-09-01 18:18:26 +0800

    add backtest qn

--- Q4.py
+++ Q4.py
@@ -1,6 +1,7 @@
 from dateutil.parser import parse
 from pandas_datareader import data as pdr
 from scipy.stats import *
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import yfinance as yf
@@ -15,6 +16,7 @@ class Portfolio(object):
 
     def __init__(self, portfolio):
         self.portfolio = portfolio
+        self.prices = pd.DataFrame()
         self.market_rtn = pd.DataFrame()
 
         columns = ['ticker', 'weight']
@@ -24,11 +26,11 @@ class Portfolio(object):
         self.portfolio.set_index('ticker', inplace=True)
         assert self.portfolio['weight'].sum() == 1
 
-    def get_returns(self,
+    def get_prices(self,
                     start_date,
                     end_date):
         """
-        Get portfolio returns between start/end date
+        Get portfolio prices between start/end date
         :param start_date: date
         :param end_date: date
         :return:
@@ -48,10 +50,21 @@ class Portfolio(object):
             if len(cl) == 0:
                 print("No data found, check ticker again.")
             else:
-                self.market_rtn[ticker] = cl.Close
+                self.prices[ticker] = cl.Close
+
+    def get_returns(self,
+                    start_date,
+                    end_date):
+        """
+        Get portfolio returns between start/end date
+        :param start_date: date
+        :param end_date: date
+        :return:
+        """
+        self.get_prices(start_date, end_date)
 
         # Calculate returns
-        self.market_rtn = self.market_rtn.apply(
+        self.market_rtn = self.prices.apply(
             lambda x: x / x.shift() - 1).fillna(0)
 
     def get_historical_var(self,
@@ -89,6 +102,56 @@ class Portfolio(object):
                norm.pdf(norm.ppf(1 - var_pct/100)) * portf_std - portf_mean
         return [f"{round(var * 100, 2)}%", f"{round(cvar * 100, 2)}%"]
 
+    def rebal(self,
+              delta=0.05,
+              lookback_days=4):
+        """
+        Risk-adjusted momentum portfolio. We o/w stocks with positive momentum,
+        and low volatility.
+
+        Assumptions:
+        1. Earliest returns available are 1 Jan 2016- no data is available prior
+        to make rebal decisions.
+        2. Portfolio starts of equal-weight all 7 assets.
+        3. Each rebal, we re-weight the assets based on how they perform
+        (+/- delta)
+
+        :param delta: float, how much to increase/decrease
+        :param lookback_days, int, lookback for momentum
+        :return: DataFrame, weights at the end of each month
+        """
+        start_date = '2016-01-01'
+        end_date = '2016-12-31'
+        num_stocks = len(self.portfolio)
+
+        assert long_days > lookback_days
+
+        if len(self.prices) == 0:
+            self.get_prices(start_date, end_date)
+
+        # Initialize result
+        dates = self.market_rtn.reset_index().groupby(
+            pd.Grouper(key='Date', freq='M')).max().index
+        result = pd.DataFrame(index=dates, columns=self.market_rtn.columns)
+        signals = pd.DataFrame(index=dates, columns=self.market_rtn.columns)
+        result.iloc[0, :] = 1.0 / result.shape[1]
+
+        for d1, d2 in zip(dates[:-1], dates[1:]):
+            subset = self.prices.loc[d1:d2].copy(deep=True)
+            signal = (subset.tail(lookback_days).pct_change().sum()) \
+                  / self.prices.loc[:d2].std()
+            signal_rank = signal.rank()
+            signals.loc[d2, :] = signal
+
+            # Get weights
+            wgt_chg = signal_rank.apply(lambda x: -delta if x <= 2 else 
+            (delta if x >= 6 else 0))
+            new_wgt = result.loc[d1] + wgt_chg
+
+            result.loc[d2, :] = new_wgt / sum(new_wgt)
+
+        return result, signals
+
 
 df = pd.DataFrame([['AAPL', 0.15],
                    ['IBM', 0.2],
@@ -107,3 +170,29 @@ q4 = Portfolio(df)
 q4.get_returns(start_date, end_date)
 print("1D Historical VaR/CVaR:", q4.get_historical_var(var_pct))
 print("1D Parametric VaR/CVaR:", q4.get_parametric_var(var_pct))
+
+
+# Risk-Adjusted Momentum Backtest with:
+# lookback_days = 3, 5, 10
+# delta = 0.05, 0.1, 0.2
+# Most optimal is a medium signal (5D lookback, 0.2 delta).
+
+strategy_returns = pd.DataFrame()
+for lookback_days in [3, 5, 10]:
+    for delta in [0.05, 0.1, 0.2]:
+        identifier = f"delta={delta},{lookback_days}D"
+
+        rebal_weights, smas = q4.rebal(delta=delta,
+                                       lookback_days=lookback_days)
+
+        # Get monthly returns plot
+
+        wgts = pd.DataFrame(index=q4.market_rtn.index).merge(
+            rebal_weights, how='outer', left_index=True, right_index=True).ffill()
+        strategy_returns[identifier] = (q4.market_rtn * wgts).sum(axis=1)
+
+# Sharpe Ratios
+print(strategy_returns.sum() / (strategy_returns.std() * np.sqrt(260)))
+
+## Plot of permutations
+strategy_returns.apply(lambda x: np.cumprod(1 + x)).plot()
